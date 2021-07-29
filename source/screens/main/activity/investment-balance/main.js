1f663c72c9b327765333515c7e40512316d759eb
Khoby-790 <ebaidoo79@gmail.com>
2021-07-29 23:04:48 +0000

    Added source/screens/main/activity/investment-balance/main.js

---
+++ source/screens/main/activity/investment-balance/main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import Colors from '../../../../constants/colors.json'
+
+const InvestmentBalance = () => {
+    return (
+        <View style={styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default InvestmentBalance
+
+const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: Colors.secondary }
+})
