8073a44a45397ef62e8fa60799cad11de6a93a2a
Khoby-790 <ebaidoo79@gmail.com>
2021-07-29 23:10:49 +0000

    Modified source/screens/main/activity/investment-balance/main.js

--- source/screens/main/activity/investment-balance/main.js
+++ source/screens/main/activity/investment-balance/main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import PageHader from '../../../../components/PageHeader/PageHader';
 import PageWrapper from '../../../../components/PageWrapper/PageWrapper';
 import Colors from '../../../../constants/colors.json'
@@ -10,7 +11,7 @@ const InvestmentBalance = () => {
             <PageHader title="Investment Account Balance" description="Your investment account balance is shown below" />
 
             <PageWrapper>
-
+                <View style={styles.alert}></View>
             </PageWrapper>
         </View>
     )
@@ -19,5 +20,6 @@ const InvestmentBalance = () => {
 export default InvestmentBalance
 
 const styles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: Colors.secondary }
+    container: { flex: 1, backgroundColor: Colors.secondary },
+    alert: { marginVertical: RFValue(25) }
 })
