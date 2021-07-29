ef1e621ad13c39d9225f63f8785a731a6b95e0f9
Khoby-790 <ebaidoo79@gmail.com>
2021-07-29 23:05:47 +0000

    Modified source/screens/main/activity/investment-balance/main.js

--- source/screens/main/activity/investment-balance/main.js
+++ source/screens/main/activity/investment-balance/main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import PageHader from '../../../../components/PageHeader/PageHader';
 import Colors from '../../../../constants/colors.json'
 
 const InvestmentBalance = () => {
     return (
         <View style={styles.container}>
-            <Text></Text>
+            <PageHader title="Investment Account Balance" />
         </View>
     )
 }
